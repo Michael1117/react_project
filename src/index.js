e272dd4a805cef024a8dd4cd180a33af2999a0d1
Michael1117 <1322720377@qq.com>
2019-03-30 01:14:24 +0800

    component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dialog from './component/Dialog.js'
 
-import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<div>
-    <Dialog content="哈哈哈哈"/>
-    <Dialog type={2} content="呵呵呵呵呵"/>
-    <Dialog type="请登录" content = {'新的jsx语法'}>
-        <button type="button" className="btn btn-success">登录</button>
-        <button type="button" className="btn btn-danger">取消</button>
-    </Dialog>
-</div>, document.querySelector('#root'));
+class Dialog extends React.Component{
+    constructor(){
+        super()
 
+        /*
+        * this.props: 属性集合
+        * this.refs: REF集合 (非受控组件)
+        * this.context: 上下文
+        * */
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>系统提示</h3>
+                <Dialog/>
+                <div></div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<div></div>, document.querySelector('#root'))
 
