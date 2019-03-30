deb2fff59fcffee1cef2444ae7fa2a95aed4f12d
Michael1117 <1322720377@qq.com>
2019-03-30 21:36:45 +0800

    复习

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import Banner from './component/Banner'
-import './static/css/reset.min.css'
-//import A5 from './static/images/5.jpg'
 
-// 在React 的jsx中需要使用图片等资源的时候 资源的地址不能使用相对地址 ()
-let img_data = [];
 
-for (let i = 1; i <= 5; i++) {
-    img_data.push({
-        id: i,
-        title: '',
-        pic: require(`./static/images/${i}.jpg`)
-    })
 
-}
+/*ReactDOM.render(<div>
+    <h2>Hello World</h2>
+</div>, document.querySelector('#root'))*/
 
+
+let name = "Michael",
+    data = [{id: 1, title: 'xxx'}, {id: 2, title: 'yyyy'}]
+
+/*
 ReactDOM.render(<div>
-    {/*data:轮播图需要绑定的数据 interval：间隔时间(3000) step:默认索引(前后各克隆一张) speed:速度(200ms)*/}
-    <Banner data={img_data} interval={3000} step={2} speed={300}/>
-    <div style={{margin: '20px auto'}}>
+    <h2>Hello World</h2>
+    <h3>{name}</h3>
+    {
+        data.map((item, index) => {
+            return <li>
+                {item.id} - {item.title}
+            </li>
+        })
+    }
+</div>, document.querySelector('#root'))*/
 
-    </div>
-    <Banner data={img_data.slice(2)} interval={5000} step={3}/>
-</div>, document.querySelector('#root'))
+ReactDOM.render(<ul></ul>, document.querySelector('#root'))
