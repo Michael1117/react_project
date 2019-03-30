3cc298dd68cebf5deaa73752a7aebd62099539c4
Michael1117 <1322720377@qq.com>
2019-03-30 14:11:09 +0800

    复合组件

--- src/index.js
+++ src/index.js
@@ -3,60 +3,58 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import PropTypes from 'prop-types'
 
-class A extends React.Component {
-    //static defaultProps = {}
-    constructor() {
-        super();
-        console.log('1=constructor')
-
-        this.state = {n: 1}
+class Head extends React.Component{
+    constructor(props) {
+        super(props)
     }
-
-    componentWillMount() {
-        console.log('2=will-mount:第一次渲染之前,componentWillMount', this.refs.HH)
-
-        setInterval(() => {
-            this.setState({
-                n: this.state.n + 1
-            })
-        }, 4000)
-    }
-
-    componentDidMount() {
-        console.log('4=Did-mount:第一次渲染之后,componentDidMount-----', this.refs.HH)
-
-    }
-
-    /*shouldComponentUpdate(nextProps, nextState, nextContext) {
-        //console.log(nextContext);
-        console.log('5=是否允许更新, shouldComponentUpdate', nextState.n);
-        if (nextState.n > 3) {
-            return false
-        }
-        return true
-    }
-
-    componentWillUpdate(nextProps, nextState, nextContext) {
-        console.log('6=组件更新之前, componentWillUpdate');
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        //console.log(snapshot);
-        console.log('8=组件更新之后, componentDidUpdate-----');
-    }*/
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        //console.log(snapshot);
-        console.log('8=组件更新之后, componentDidUpdate-----');
-    }
-
 
     render() {
-        console.log('render');
-        return <div ref='HH'>
-            {this.state.n}
-        </div>
+        return (
+            <div className="panel-heading">
+                <h3 className="panel-title">
+                    点击次数：{this.props.count}
+                </h3>
+            </div>
+        );
     }
 }
 
 
-ReactDOM.render(<A/>, document.querySelector('#root'))
+class Body extends React.Component{
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="panel-body">
+                <button className="btn btn-success" onClick={this.props.callBack}>点我啊</button>
+            </div>
+        );
+    }
+}
+
+class Panel extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            n: 0
+        }
+    }
+
+    fn = () => {
+        // => 修改panel的状态信息
+        this.setState({
+            n: ++this.state.n
+        })
+    }
+    render() {
+        return <section className="panel panel-default" style={{width: '50%', margin: '20px auto'}}>
+            <Head count={this.state.n}/>
+            {/*父组件*/}
+            <Body callBack={this.fn}/>
+        </section>
+    }
+}
+
+ReactDOM.render(<Panel/>, document.querySelector('#root'))
